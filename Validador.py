d511eb009c4e4afc1541a3d2ca32d506d27593c1
Yuri Shinohara <142069309+rdv123456@users.noreply.github.com>
2025-06-06 23:10:30 -0300

    Create Validador.py

---
+++ Validador.py
@@ -0,0 +1,48 @@
+expressoes = [
+    "(a+b)*(c-d)",     # Válida
+    "((x+y)*(z/2))",   # Válida
+    "(a+b)*c)",        # Inválida - parêntese a mais
+    "((a+b)*(c-d)",    # Inválida - parêntese a menos
+    "a++b",            # Inválida - operador duplicado
+    "a+b*(c/d)",       # Válida
+]
+
+
+
+
+def validar_expressao(expressao str) - bool
+    pilha = []
+    operadores = set(+-)
+    operandos = set(abcdefghijklmnopqrstuvwxyz0123456789)
+    ultimo = None  
+    # último tipo de símbolo lido 'op', 'num', '(', ')'
+
+    for i, char in enumerate(expressao)
+        if char == ' '
+            continue
+        elif char == '('
+            pilha.append(char)
+            ultimo = '('
+        elif char == ')'
+            if not pilha or pilha[-1] != '('
+                return False
+            pilha.pop()
+            if ultimo in operadores or ultimo == '('
+                return False
+            ultimo = ')'
+        elif char in operandos
+            if ultimo in operandos or ultimo == ')'
+                return False
+            ultimo = 'num'
+        elif char in operadores
+            if ultimo not in ['num', ')']
+                return False
+            ultimo = 'op'
+        else
+            return False  # símbolo inválido
+
+    return len(pilha) == 0 and ultimo not in operadores and ultimo is not None
+
+
+for exp in expressoes:
+    print(f"{exp}: {'Válida' if validar_expressao(exp) else 'Inválida'}")
